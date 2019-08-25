bed36b9255acd7835fac5351e82b89abdc9d8858
Leticia Campos <leticia.silva105@fatec.sp.gov.br>
2019-08-25 18:42:20 -0300

    Passando a anotacao do request para o controller

--- empresta-livro-api/empresta-livro-api/src/main/java/com/djg/emprestalivro/emprestalivroapi/controller/EmprestimoController.java
+++ empresta-livro-api/empresta-livro-api/src/main/java/com/djg/emprestalivro/emprestalivroapi/controller/EmprestimoController.java
@@ -2,6 +2,7 @@ package com.djg.emprestalivro.emprestalivroapi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.djg.emprestalivro.emprestalivroapi.servico.EmprestimoServico;
@@ -14,7 +15,7 @@ public class EmprestimoController {
 	private EmprestimoServico emprestimoServico;
 	
 	@PostMapping("/emprestimo")
-	public void salvaEmprestimo(EmprestimoVO emprestimo) {
+	public void salvaEmprestimo(@RequestBody EmprestimoVO emprestimo) {
 		emprestimoServico.salvaEmprestimo(emprestimo);
 	}
 
